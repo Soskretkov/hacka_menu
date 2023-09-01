8909829f70f64d5e5272b82c447c0b719fe344ec
Soskretkov <oskrserg@gmail.com>
2023-09-01 17:20:28 +0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import './styles.css'
+
+import { BackgroundModule } from './modules/background.module';
+
+// Создаём экземпляр модуля
+new BackgroundModule().trigger();
---
+++ src/modules/background.module.js
@@ -0,0 +1,14 @@
+import { Module } from '../core/module'
+import { getRandomColor } from '../utils.js'
+
+
+export class BackgroundModule extends Module {
+    constructor() {
+      super('background', 'Поменять цвет');
+    }
+  
+    trigger() {
+      document.body.style.backgroundColor = getRandomColor();
+    }
+  }
+  
