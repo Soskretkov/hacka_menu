4cfccf583b71ad8da0093be0bcac98b093c28f02
Soskretkov <oskrserg@gmail.com>
2023-09-02 09:37:45 +0300

    fixed bug in background.module.js

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,6 +1,6 @@
 import { Module } from '../core/module'
 import { getRandomColor } from '../utils.js'
-import { setGlobalSettings } from '../localStorageAPI.js'
+import { setGlobalSettings } from '../localStorage.js'
 
 
 export class BackgroundModule extends Module {
